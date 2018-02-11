731be5457a13116a404f45e63854731796787ffa
lucidmaj7 <lucidmaj7@gmail.com>
2018-02-11 20:01:20 +1030

    add views

--- routes/files.js
+++ routes/files.js
@@ -2,6 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
+router.get('/', function(req, res, next) {
+       
+        res.render('files', { title: "" });
+});
+
 router.get('/:dir(*)', function(req, res, next) {
 	var dir = req.params.dir;
 	res.render('files', { title: dir });
